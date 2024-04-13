9fc117051cc99d33ddcb67fa17c31926b8ce9bd1
bagas aditya <bagasaditya599@gmail.com>
2024-04-14 02:47:17 +0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { Suspense } from "react";
+import Loading from "./loading";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={"bg-color-secondary"} suppressHydrationWarning={true}>
         {/* <Navbar /> */}
+        <Suspense fallback={Loading}></Suspense>
         {children}
       </body>
     </html>
